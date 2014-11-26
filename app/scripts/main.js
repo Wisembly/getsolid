d75cbf81a5d9fa457f196a02b54c341a786e75b1
Tristan Daeschner <tristan.daeschner@gmail.com>
2014-11-26 12:00:39 +0100

    - update header image - make prefinery call work with multiple buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-$('#prefinery_second_button');
+
+$(function () {
+  var prefinery_form_url =
+    'https://' + Prefinery.konfig.account +
+    '.prefinery.com/betas/' + Prefinery.konfig.beta_id +
+    '/testers/new?display=popup&version=' + Prefinery.konfig.version;
+
+  $(document).on('click', '.show-prefinery-form', function () {
+    Prefinery.show(url);
+  });
+})
