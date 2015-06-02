d0dcf14a8b288abd7e0c28d1cfd217a5882946cb
Tristan Daeschner <tristan.daeschner@gmail.com>
2015-06-02 18:00:50 +0200

    - added numa custom message

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -456,6 +456,24 @@ $(document).ready(function () {
             }
           }
         ],
+        'hello-numa': [
+          {
+            'el': $('#cta'),
+            'method': 'html',
+            'value': {
+              'fr': '<a class="btn btn-default i18n" href="http://solid.wisembly.com">Créez votre compte</a>',
+              'en': '<a class="btn btn-default i18n" href="http://solid.wisembly.com">Create your account</a>'
+            }
+          },
+          {
+            'el': $('.notification .container'),
+            'method': 'html',
+            'value': {
+              'fr': "👋 Bonjour <strong>Numa</strong>! Accédez avant tout le monde à notre beta privée ci-dessous.",
+              'en': "👋 Hello <strong>Numa</strong>! Get instant access to our private beta below."
+            }
+          }
+        ],
     };
 
     var testCatalog = function (catalog) {
